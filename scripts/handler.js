2a3055d336d3543e0edf1b5a89fa507310160eaf
Vincenzo Deluca <delucaavincenzo@gmail.com>
2024-01-02 19:07:10 +0100

    Update code length validation

--- scripts/handler.js
+++ scripts/handler.js
@@ -2,7 +2,7 @@ import { createDisplay } from "/scripts/db.js";
 
 document.getElementById('connectDisplay').addEventListener('click', function(e) {
     var code = document.getElementById('code-d').value;
-    if (code.length === 10) {
+    if (code.length === 15) {
         window.location.href = "/display?id="+ code
     } else {
         alert("Invalid Code")
@@ -10,7 +10,7 @@ document.getElementById('connectDisplay').addEventListener('click', function(e) 
   });
 document.getElementById('connectManage').addEventListener('click', function(e) {
 var code = document.getElementById('code-m').value;
-if (code.length === 10) {
+if (code.length === 15) {
     window.location.href = "/manage?id="+ code
 } else if (code === "") {
     var result = confirm("Do you want to create a new Display?")
